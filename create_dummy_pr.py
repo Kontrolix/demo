2b6e13de612bf224466c79c95e472b09773fff5c
Kontrolix <damien.couppe@mergify.com>
2023-07-31 16:00:36 +0200

    feat: A new feature

--- create_dummy_pr.py
+++ create_dummy_pr.py
@@ -7,25 +7,35 @@ pr_title = ["feat: A new feature", "fix: a fix", "chore: typo"]
 graphql_query_template = 'i{}: createPullRequest(input: {{ baseRefName: "main", headRefName: "{}", repositoryId: $repo_id, title: "{}" }}){{pullRequest{{ url }} }}'
 graphql_query = []
 try:
-    for i in range(55):
+    for i in range(3):
         branch = f"{int(time.time())}_{i}"
 
         title = random.choice(pr_title)
 
-        # subprocess.check_call(["git", "checkout", "-b", branch])
+        subprocess.check_call(["git", "checkout", "-b", branch])
 
-        # with open(f"dummy_files/{branch}.txt", "w") as f:
-        #     f.write("foo")
+        with open(f"dummy_files/{branch}.txt", "w") as f:
+            f.write("foo")
 
-        # subprocess.check_call(["git", "add", "--all"])
-        # subprocess.check_call(["git", "commit", "-a", "-m", title])
+        subprocess.check_call(["git", "add", "--all"])
+        subprocess.check_call(["git", "commit", "-a", "-m", title])
 
-        # subprocess.call(["git", "push", "--set-upstream", "origin", branch])
+        subprocess.call(["git", "push", "--set-upstream", "origin", branch])
 
-        # graphql_query.append(graphql_query_template.format(i, branch, title))
+        graphql_query.append(graphql_query_template.format(i, branch, title))
 
     with open("too-quickly.graphql", "w") as f:
-        f.write(f"mutation($repo_id: ID!) {{','.join(graphql_query)}}")
-    # gh api graphql -F query=@too-quickly.graphql -f repo_id="$(gh repo view "Kontrolix/demo" --json id -q .id)"
+        f.write(f"mutation($repo_id: ID!) {{{','.join(graphql_query)}}}")
+    subprocess.call(
+        [
+            "gh",
+            "api",
+            "graphql",
+            "-F",
+            "query=@too-quickly.graphql",
+            "-f",
+            'repo_id="$(gh repo view "Kontrolix/demo" --json id -q .id)"',
+        ]
+    )
 finally:
     subprocess.check_call(["git", "checkout", "main"])
